4be457e73ff89d8f2176064f893b03726a554ba4
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2024-11-03 20:25:21 -0300

    fix: errores abm jugador

--- src/database/AbmJugador.py
+++ src/database/AbmJugador.py
@@ -1,5 +1,6 @@
 from DaoInterfaz import DaoInterfaz
 from Singleton import Database
+from Jugador import Jugador
 
 
 class AbmJugador(DaoInterfaz):
@@ -7,22 +8,40 @@ class AbmJugador(DaoInterfaz):
         self.__database = Database()
         self.__database.connect()
 
-    def get_por_id(self, id: int) -> tuple:
+    def get_por_id(self, id: int) -> tuple:  # ESTO ANDA
         resultado = self.__database.execute_query(
-            "SELECT * FROM usuario WHERE id_usuario = ? AND baja_usuario = 0", (id,)
+            "SELECT * FROM usuario WHERE id_usuario = ? AND baja_usuario = 0",
+            (id,),  # Devuelve una lista de tuplas
         )
-        return (
-            resultado[0]
-            if resultado
-            else print(f"No se encontro el usuario con ese id: {id}")
-        )  # Devuelve el primer elemento de la lista si hay resultados, sino None
+        if not resultado:  # Si no se encontro la id o esta dado de baja, devuelve una lista vacia(Aunque solo busques un usuario) y se imprime un mensaje.
+            print(f"No se encontro el usuario con la id: {id} o esta dado de baja")
+        else:
+            return Jugador(
+                resultado[0][0],
+                resultado[0][1],
+                resultado[0][2],
+                resultado[0][3],
+                resultado[0][4],
+            )
 
-    def get_all(self):
-        return self.__database.execute_query(
+    def get_all(self):  # ESTO ANDA
+        jugadores = []
+        resultado = self.__database.execute_query(
             "SELECT * FROM usuario WHERE baja_usuario = 0"
         )
+        if (
+            resultado is None
+        ):  # Si no se encontro la id, devuelve None y se imprime un mensaje.
+            print("No se encontraron usuarios")
+        else:
+            for jugador in resultado:
+                objeto = Jugador(
+                    jugador[0], jugador[1], jugador[2], jugador[3], jugador[4]
+                )
+                jugadores.append(objeto)
+            return jugadores
 
-    def insertar(self, objeto):
+    def insertar(self, objeto):  # ESTO ANDA
         self.__database.execute_non_query(
             "INSERT INTO usuario (id_usuario, nombre_usuario, password, admin, baja_usuario) VALUES (?,?,?,?,?)",
             (
@@ -34,7 +53,7 @@ class AbmJugador(DaoInterfaz):
             ),
         )
 
-    def actualizar(self, objeto):
+    def actualizar(self, objeto):  # ESTO ANDA
         self.__database.execute_non_query(
             "UPDATE usuario SET nombre_usuario = ?, password = ?, admin = ?, baja_usuario = ? WHERE id_usuario = ?",
             (
@@ -46,8 +65,11 @@ class AbmJugador(DaoInterfaz):
             ),
         )
 
-    def borrar(self, id):
+    def borrar(self, id):  # ESTO ANDA
         self.__database.execute_non_query(
             "UPDATE usuario SET baja_usuario = 1 WHERE id_usuario = ? AND baja_usuario = 0",
             (id,),
         )
+
+    def close(self):  # ESTO ANDA
+        self.__database.close_connection()
---
+++ src/database/prueba.py
@@ -0,0 +1,5 @@
+from AbmJugador import AbmJugador
+
+abm = AbmJugador()
+jugador = abm.get_por_id(1)
+abm.close()
