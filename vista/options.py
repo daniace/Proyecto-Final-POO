48bfb8b0cbf0e66c8bdf75104f0472c045d1c1fb
hachazo <87842940+hachazo@users.noreply.github.com>
2024-11-03 20:03:29 -0300

    up

--- vista/options.py
+++ vista/options.py
@@ -13,6 +13,7 @@ pygame.display.set_caption("Héroes del Balón")
 WHITE = (255, 255, 255)
 BUTTON_COLOR = (200, 200, 170)
 BUTTON_HOVER_COLOR = (180, 180, 150)
+radio_borde = 5
 
 # Cargar imagen de fondo
 background_image = pygame.image.load(r"vista\options.png")
@@ -27,9 +28,9 @@ def draw_button(text, x, y, w, h, color, hover_color):
     button_rect = pygame.Rect(x, y, w, h)
     
     if button_rect.collidepoint(mouse_pos):
-        pygame.draw.rect(screen, hover_color, button_rect)
+        pygame.draw.rect(screen, hover_color, button_rect,border_radius=radio_borde)
     else:
-        pygame.draw.rect(screen, color, button_rect)
+        pygame.draw.rect(screen, color, button_rect,border_radius=radio_borde)
     
     text_surface = font.render(text, True, WHITE)
     text_rect = text_surface.get_rect(center=(x + w / 2, y + h / 2))
@@ -43,11 +44,15 @@ while running:
     screen.blit(background_image, (0, 0))  # Dibujar imagen de fondo
     
     # Dibujar botones
-    volver_button = draw_button("Volver", WIDTH // 2 - 350, HEIGHT // 2 - 250, 100, 50, BUTTON_COLOR, BUTTON_HOVER_COLOR)
-    play_button = draw_button("Dificultad", WIDTH // 2 - 300, HEIGHT // 2 - 130, 200, 50, BUTTON_COLOR, BUTTON_HOVER_COLOR)
-    options_button = draw_button("Sonido", WIDTH // 2 - 300, HEIGHT // 2 - 17, 200, 50, BUTTON_COLOR, BUTTON_HOVER_COLOR)
-    quit_button = draw_button("Controles", WIDTH // 2 - 300, HEIGHT // 2 + 90, 200, 50, BUTTON_COLOR, BUTTON_HOVER_COLOR)
-    
+    volver_button = draw_button("Volver", WIDTH // 2 - 350, HEIGHT // 2 - 250, 100, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    play_button = draw_button("Dificultad", WIDTH // 2 - 300, HEIGHT // 2 - 130, 130, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    options_button = draw_button("Sonido", WIDTH // 2 - 300, HEIGHT // 2 - 17, 130, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    quit_button = draw_button("Controles", WIDTH // 2 - 300, HEIGHT // 2 + 90, 130, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    facil_button = draw_button("Facil", WIDTH // 2 - 150, HEIGHT // 2 - 130, 100, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    normal_button = draw_button("Normal", WIDTH // 2 - 20, HEIGHT // 2 - 130, 100, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    dificil_button = draw_button("Dificil", WIDTH // 2 + 105, HEIGHT // 2 - 130, 100, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    sonido_on_button = draw_button("Encendido", WIDTH // 2 - 90, HEIGHT // 2 - 17, 130, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
+    sonido_off_button = draw_button("apagado", WIDTH // 2 + 60, HEIGHT // 2 - 17, 130, 40, BUTTON_COLOR, BUTTON_HOVER_COLOR)
     
     # Eventos
     for event in pygame.event.get():
