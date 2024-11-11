78e9a84903254ac22ee24fc27cfa5e1abb8671ec
BrunoAguinaga <brunoaguinaga0@gmail.com>
2024-11-11 16:26:42 -0300

    Correciones ABM carta
    Corregi el abm carta - get por id y get all ya que devolvia una tupla y no objetos

--- src/database/prueba.py
+++ src/database/prueba.py
@@ -6,34 +6,34 @@ from Equipo import Equipo
 
 abmC = AbmCarta()
 jugador = abmC.get_por_id(41)
-print(jugador)
+print(jugador, type(jugador))
 # abm.close()
 
 ########## ABM EQUIPO ##########
 
-# Creación de equipo
-equipo2 = Equipo(200, "equipo2", 2)
+# # Creación de equipo
+# equipo2 = Equipo(200, "equipo2", 2)
 
-# abm equipo
-abmE = AbmEquipo()
+# # abm equipo
+# abmE = AbmEquipo()
 
-# Agregar equipo a la BDD
-# abmE.insertar(equipo2)
+# # Agregar equipo a la BDD
+# # abmE.insertar(equipo2)
 
-# Devuelve el equipo por id
-equipo = abmE.get_por_id(200)
-print(equipo.get_id_equipo())  # imprimo su id
+# # Devuelve el equipo por id
+# equipo = abmE.get_por_id(200)
+# print(equipo.get_id_equipo())  # imprimo su id
 
-# Devuelve todos los equipos
-equipos = abmE.get_all()
-for equipo in equipos:  # imprimo todos sus ids
-    print(equipo.get_id_equipo())
+# # Devuelve todos los equipos
+# equipos = abmE.get_all()
+# for equipo in equipos:  # imprimo todos sus ids
+#     print(equipo.get_id_equipo())
 
-# Actualizar equipo en la BDD
-equipo2_actualizado = Equipo(
-    200, "equipo2.0", 2
-)  # el mismo id pero le cambio el nombre
-abmE.actualizar(equipo2_actualizado)
+# # Actualizar equipo en la BDD
+# equipo2_actualizado = Equipo(
+#     200, "equipo2.0", 2
+# )  # el mismo id pero le cambio el nombre
+# abmE.actualizar(equipo2_actualizado)
 
-# Borrado lógico de equipo en la BDD
-# abmE.borrar(200)
+# # Borrado lógico de equipo en la BDD
+# # abmE.borrar(200)
