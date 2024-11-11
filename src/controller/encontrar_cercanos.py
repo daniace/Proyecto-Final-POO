fc61379c0cd0bf1cc44b5aa1c46c25af9636de6f
LEONEL <leonelcnr@gmail.com>
2024-11-11 19:13:18 -0300

    Funcion para encontrar jugadores cercanos

--- src/controller/encontrar_cercanos.py
+++ src/controller/encontrar_cercanos.py
@@ -1,10 +1,16 @@
 from collections import deque
 
-def encontrar_dos_mas_cercano(matriz, punto_referencia):
+
+def encontrar_jugadores(matriz, punto_referencia, numero):
     filas = len(matriz)
     columnas = len(matriz[0])
     visitado = [[False for _ in range(columnas)] for _ in range(filas)]
-    movimientos = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Movimientos posibles: arriba, abajo, izquierda, derecha
+    movimientos = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+    ]  # Movimientos posibles: arriba, abajo, izquierda, derecha
 
     fila_inicio, columna_inicio = punto_referencia
     cola = deque([(fila_inicio, columna_inicio, 0)])  # (fila, columna, distancia)
@@ -16,7 +22,7 @@ def encontrar_dos_mas_cercano(matriz, punto_referencia):
         fila_actual, columna_actual, distancia = cola.popleft()
 
         # Verificar si encontramos un 1
-        if matriz[fila_actual][columna_actual] == 1:
+        if matriz[fila_actual][columna_actual] == numero:
             puntos_cercanos.append(((fila_actual, columna_actual), distancia))
             if len(puntos_cercanos) > 3:
                 puntos_cercanos.sort(key=lambda x: x[1])
@@ -26,20 +32,30 @@ def encontrar_dos_mas_cercano(matriz, punto_referencia):
             nueva_fila = fila_actual + movimiento[0]
             nueva_columna = columna_actual + movimiento[1]
 
-            if 0 <= nueva_fila < filas and 0 <= nueva_columna < columnas and not visitado[nueva_fila][nueva_columna]:
+            if (
+                0 <= nueva_fila < filas
+                and 0 <= nueva_columna < columnas
+                and not visitado[nueva_fila][nueva_columna]
+            ):
                 visitado[nueva_fila][nueva_columna] = True
                 cola.append((nueva_fila, nueva_columna, distancia + 1))
     return puntos_cercanos
 
+
 # Si no se encuentra un 1 en la matriz
 matriz = [
-    [0, 0, 0, 1],
-    [0, 0, 0, 0],
-    [0, 0, 1, 0],
-    [1, 0, 0, 0]
+    [0, 1, 0, 0, 2, 0, 2, 0],
+    [0, 1, 2, 1, 2, 1, 2, 0],
+    [1, 0, 0, 1, 0, 1, 0, 2],
+    [0, 1, 2, 1, 2, 1, 2, 0],
+    [0, 1, 0, 0, 2, 0, 2, 0],
 ]
 
 punto_referencia = (0, 0)
-puntos_cercanos = encontrar_dos_mas_cercano(matriz, punto_referencia)
+puntos_cercanos = encontrar_jugadores(matriz, punto_referencia, 1)
 for punto, distancia in puntos_cercanos:
     print(f"El 1 está en la posición {punto} a una distancia de {distancia}")
+print()
+puntos_cercanos = encontrar_jugadores(matriz, punto_referencia, 2)
+for punto, distancia in puntos_cercanos:
+    print(f"El 2 está en la posición {punto} a una distancia de {distancia}")
