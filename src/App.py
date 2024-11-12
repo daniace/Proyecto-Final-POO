d1ab19ba70c634a3d243a0faa1debb6361abd701
daniel_enegron <danielenegron2018@gmail.com>
2024-11-12 20:24:32 -0300

    update imag's

--- src/App.py
+++ src/App.py
@@ -147,7 +147,7 @@ def opciones():
         control2_img = pygame.transform.scale(control2_img, (300, 300))
 
         SCREEN.blit(control1_img, (int(ANCHO * 0.3), int(ALTO * 0.4)))
-        SCREEN.blit(control2_img, (int(ANCHO * 0.7), int(ALTO * 0.4)))
+        SCREEN.blit(control2_img, (int(ANCHO * 0.54), int(ALTO * 0.44)))
 
         FACIL = Boton(
             boton_surface,
