f7f299a446ea1546aca22d74f4eaac178c922b5a
Daniel Emiliano Acevedo <danielace@proton.me>
2024-11-13 16:51:10 -0300

    feat: :lipstick: Se hizo la simulacion de eleccion de cartas y tabla de rankings

--- src/view/app.py
+++ src/view/app.py
@@ -2,8 +2,11 @@ import sys
 
 import pygame
 
-from .settings import *
+from database.AbmCarta import AbmCarta
+from database.AbmUsuario import AbmUsuario
+
 from .Boton import *
+from .settings import *
 
 # pygame setup
 pygame.init()
@@ -16,8 +19,6 @@ bg_opciones = pygame.image.load(IMAGEN_FONDO_OPCIONES)
 BG_OPCIONES = pygame.transform.scale(bg_opciones, TAMANIO_PANTALLA)
 bg_jugar = pygame.image.load(IMAGEN_FONDO)
 BG_JUGAR = pygame.transform.scale(bg_jugar, TAMANIO_PANTALLA)
-bg_formacion = pygame.image.load(IMAGEN_FORMACION)
-BG_FORMACION = pygame.transform.scale(bg_formacion, TAMANIO_PANTALLA)
 bg_ranking = pygame.image.load(IMAGEN_RANKING)
 BG_RANKING = pygame.transform.scale(bg_ranking, TAMANIO_PANTALLA)
 pygame.mixer.init()
@@ -35,18 +36,105 @@ clock = pygame.time.Clock()
 boton_surface = pygame.image.load(IMAGEN_BOTON4)
 boton_surface = pygame.transform.scale(boton_surface, (250, 80))
 
+abmcarta = AbmCarta()
+defensores = abmcarta.get_defensores()
+
 
 def jugar():
     pygame.display.set_caption("JUGANDO")
 
     while True:
         JUGAR_POS_MOUSE = pygame.mouse.get_pos()
-        SCREEN.blit(BG_FORMACION, (0, 0))
-        # SCREEN.fill("black")
-
+        SCREEN.fill(NEGRO)
+        OFFSET = 40
         TEXTO_JUGAR = get_fuente(45).render("VENTANA JUGANDO", True, "White")
         JUGAR_RECT = TEXTO_JUGAR.get_rect(center=(ANCHO // 2, 50))
         SCREEN.blit(TEXTO_JUGAR, JUGAR_RECT)
+
+        CANCHA_IMAGEN = pygame.image.load(IMAGEN_CANCHA)
+        CANCHA_IMAGEN = pygame.transform.scale(CANCHA_IMAGEN, (ANCHO // 2, ALTO))
+        SCREEN.blit(CANCHA_IMAGEN, (int(ANCHO * 0.25), 0))
+
+        # Formacion 4-3-3
+        CARTA_IMAGEN = pygame.image.load(IMAGEN_CARTA)
+        CARTA_IMAGEN = pygame.transform.scale(CARTA_IMAGEN, (80, 120))
+        DELANTERO1_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.4), int(ALTO * 0.3))
+        )
+        DELANTERO2_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.5), int(ALTO * 0.3))
+        )
+        DELANTERO3_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.6), int(ALTO * 0.3))
+        )
+        MEDIOCAMPISTA1_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.4), int(ALTO * 0.5))
+        )
+        MEDIOCAMPISTA2_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.5), int(ALTO * 0.5))
+        )
+        MEDIOCAMPISTA3_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.6), int(ALTO * 0.5))
+        )
+        DEFENSA1_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.35), int(ALTO * 0.7))
+        )
+        DEFENSA2_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.45), int(ALTO * 0.7))
+        )
+        DEFENSA3_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.55), int(ALTO * 0.7))
+        )
+        DEFENSA4_RECT = CARTA_IMAGEN.get_rect(
+            center=(int(ANCHO * 0.65), int(ALTO * 0.7))
+        )
+        PORTERO_RECT = CARTA_IMAGEN.get_rect(center=(int(ANCHO * 0.5), int(ALTO * 0.9)))
+        NOMBRE_JUGADOR = get_fuente(20).render(defensores[0].get_str(), True, NEGRO)
+        NOMBRE_DELANTERO1 = NOMBRE_JUGADOR.get_rect(center=DELANTERO1_RECT.center)
+        NOMBRE_DELANTERO2 = NOMBRE_JUGADOR.get_rect(center=DELANTERO2_RECT.center)
+        NOMBRE_DELANTERO3 = NOMBRE_JUGADOR.get_rect(center=DELANTERO3_RECT.center)
+        NOMBRE_MEDIOCAMPISTA1 = NOMBRE_JUGADOR.get_rect(
+            center=MEDIOCAMPISTA1_RECT.center
+        )
+        NOMBRE_MEDIOCAMPISTA2 = NOMBRE_JUGADOR.get_rect(
+            center=MEDIOCAMPISTA2_RECT.center
+        )
+        NOMBRE_MEDIOCAMPISTA3 = NOMBRE_JUGADOR.get_rect(
+            center=MEDIOCAMPISTA3_RECT.center
+        )
+        NOMBRE_DEFENSA1 = NOMBRE_JUGADOR.get_rect(center=DEFENSA1_RECT.center)
+        NOMBRE_DEFENSA2 = NOMBRE_JUGADOR.get_rect(center=DEFENSA2_RECT.center)
+        NOMBRE_DEFENSA3 = NOMBRE_JUGADOR.get_rect(center=DEFENSA3_RECT.center)
+        NOMBRE_DEFENSA4 = NOMBRE_JUGADOR.get_rect(center=DEFENSA4_RECT.center)
+        NOMBRE_PORTERO = NOMBRE_JUGADOR.get_rect(center=PORTERO_RECT.center)
+
+        # Delanteros
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.4) - OFFSET, int(ALTO * 0.2)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.5) - OFFSET, int(ALTO * 0.2)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.6) - OFFSET, int(ALTO * 0.2)))
+        # Mediocampistas
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.4) - OFFSET, int(ALTO * 0.4)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.5) - OFFSET, int(ALTO * 0.4)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.6) - OFFSET, int(ALTO * 0.4)))
+        # Defensores
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.35) - OFFSET, int(ALTO * 0.6)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.45) - OFFSET, int(ALTO * 0.6)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.55) - OFFSET, int(ALTO * 0.6)))
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.65) - OFFSET, int(ALTO * 0.6)))
+        # Portero
+        SCREEN.blit(CARTA_IMAGEN, (int(ANCHO * 0.5) - OFFSET, int(ALTO * 0.8)))
+
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DELANTERO1)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DELANTERO2)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DELANTERO3)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_MEDIOCAMPISTA1)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_MEDIOCAMPISTA2)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_MEDIOCAMPISTA3)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DEFENSA1)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DEFENSA2)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DEFENSA3)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_DEFENSA4)
+        SCREEN.blit(NOMBRE_JUGADOR, NOMBRE_PORTERO)
 
         JUGAR_ATRAS = Boton(
             boton_surface,
@@ -59,16 +147,15 @@ def jugar():
         JUGAR_ATRAS.changeColor(JUGAR_POS_MOUSE)
         JUGAR_ATRAS.update(SCREEN)
         # ------------
-        JUGAR_FORMACION = Boton(
-            None,
-            (ANCHO // 4.49 - 100, ALTO // 3.2 - 130),
-            "FORMACIONES:",
-            get_fuente(75),
-            "White",
-            "Green",
+        COLOR_FONDO = (128, 128, 128)
+        TEXTO_FORMACION = get_fuente(75).render("FORMACION 4-3-3", True, "White")
+        FORMACION_RECT = TEXTO_FORMACION.get_rect(
+            center=(int(ANCHO * 0.5), int(ALTO * 0.1))
         )
-        JUGAR_FORMACION.changeColor(JUGAR_POS_MOUSE)
-        JUGAR_FORMACION.update(SCREEN)
+        margen = 20
+        fondo_rect = FORMACION_RECT.inflate(margen * 2, margen * 2)
+        pygame.draw.rect(SCREEN, COLOR_FONDO, fondo_rect, border_radius=15)
+        SCREEN.blit(TEXTO_FORMACION, FORMACION_RECT)
         # ------------
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -255,15 +342,33 @@ def opciones():
         pygame.display.update()
 
 
+abmusuario = AbmUsuario()
+usuarios = abmusuario.get_all()
+
+
 def ranking():
     while True:
         SCREEN.fill("black")
 
-        TEXTO_RANKING = get_fuente(45).render("VENTANA RANKING", True, "White")
-        RANKING_RECT = TEXTO_RANKING.get_rect(center=(ANCHO // 2, 50))
-        SCREEN.blit(TEXTO_RANKING, RANKING_RECT)
         SCREEN.blit(BG_RANKING, (0, 0))
         RANKING_POS_MOUSE = pygame.mouse.get_pos()
+
+        IMAGEN_RANKING_USUARIOS = pygame.image.load(IMAGEN_TABLA)
+        IMAGEN_RANKING_USUARIOS = pygame.transform.scale(
+            IMAGEN_RANKING_USUARIOS, (ANCHO // 3, ALTO - 100)
+        )
+        SCREEN.blit(IMAGEN_RANKING_USUARIOS, (int(ANCHO * 0.35), int(ALTO * 0.1)))
+
+        for i, usuario in enumerate(usuarios):
+            texto = f"{i + 1}. {usuario.get_nombre()} - {usuario.get_score()}"
+            texto_usuario = get_fuente(72).render(texto, True, NEGRO)
+            SCREEN.blit(texto_usuario, (ANCHO // 2 - 120, 170 + i * 50))
+
+        TEXTO_TABLA = get_fuente(50).render("USUARIO - PUNTUACION", True, NEGRO)
+        TEXTO_TABLA_RECT = TEXTO_TABLA.get_rect(
+            center=(int(ANCHO * 0.52), int(ALTO * 0.2))
+        )
+        SCREEN.blit(TEXTO_TABLA, TEXTO_TABLA_RECT)
 
         RANKING_ATRAS = Boton(
             boton_surface,
