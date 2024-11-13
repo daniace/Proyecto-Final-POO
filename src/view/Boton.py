6cab16da847548fd9ef66eab59c06269c4b92fa5
daniel_enegron <danielenegron2018@gmail.com>
2024-11-13 16:36:15 -0300

    b-->B

--- src/view/Boton.py
+++ src/view/Boton.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class boton:
+class Boton:
     def __init__(self, image, pos, text_input, font, base_color, hovering_color):
         self.imagen = image
         self.x_pos = pos[0]
