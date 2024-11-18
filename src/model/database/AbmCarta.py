2be9935197ed3d7b565d30e637ea4df23b53f41b
dukireal <130255243+dukireal@users.noreply.github.com>
2024-11-18 14:42:31 -0300

    update path

--- src/model/database/AbmCarta.py
+++ src/model/database/AbmCarta.py
@@ -1,3 +1,6 @@
+import os 
+import sys 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from model.database.Carta import Carta
 from model.database.DaoInterfaz import DaoInterfaz
 from model.database.Singleton import Database
