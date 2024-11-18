d81c2a463b8c489bff51df6c7a2803e19e404405
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2024-11-17 22:41:47 -0300

    borre 2 lineas

--- src/Heroes_Del_Balon.py
+++ src/Heroes_Del_Balon.py
@@ -1,8 +1,6 @@
 import sys
 import pygame as pg
 from controller.MenuViewControlador import MenuController
-from view.MenuView import MenuView
-from settings import ANCHO, ALTO
 
 if __name__ == "__main__":
     menu_principal = MenuController()
