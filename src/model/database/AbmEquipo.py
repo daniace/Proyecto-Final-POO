e1c07dfbec1df7d17e98c70c0564774a2ef06fc1
dimidio6 <angelo.d6@outlook.com>
2024-11-21 14:20:43 -0300

    refactor: :card_file_box: Equipo y AbmEquipo, cartas agregadas

--- src/model/database/AbmEquipo.py
+++ src/model/database/AbmEquipo.py
@@ -16,8 +16,21 @@ class AbmEquipo(DaoInterfaz):
             print(f"No se encontró el equipo con el id: {id}")
         else:
             return Equipo(
-                resultado[0][0], resultado[0][1], resultado[0][2]
-            )  # Retorna el equipo con sus 3 atributos
+                resultado[0][0],
+                resultado[0][1],
+                resultado[0][2],
+                resultado[0][3],
+                resultado[0][4],
+                resultado[0][5],
+                resultado[0][6],
+                resultado[0][7],
+                resultado[0][8],
+                resultado[0][9],
+                resultado[0][10],
+                resultado[0][11],
+                resultado[0][12],
+                resultado[0][13],
+            )  # Retorna el objeto equipo
 
     def get_all(self):  # FUNCIONA
         equipos = []
@@ -29,21 +42,64 @@ class AbmEquipo(DaoInterfaz):
         else:
             for equipo in resultado:
                 objeto = Equipo(
-                    equipo[0], equipo[1], equipo[2]
-                )  # creo un objeto Equipo para cada índice de la lista, con sus parámetros: 0, 1, 2
+                    equipo[0],
+                    equipo[1],
+                    equipo[2],
+                    equipo[3],
+                    equipo[4],
+                    equipo[5],
+                    equipo[6],
+                    equipo[7],
+                    equipo[8],
+                    equipo[9],
+                    equipo[10],
+                    equipo[11],
+                    equipo[12],
+                    equipo[13],
+                )  # creo un objeto Equipo para cada índice de la lista
                 equipos.append(objeto)
             return equipos
 
     def insertar(self, objeto):  # FUNCIONA
         self.__database.execute_non_query(
-            "INSERT INTO equipo (id_equipo, id_usuario, nombre_equipo, id_carta1, id_carta2, id_carta3, id_carta4, id_carta6, id_carta7_, id_carta8, id_carta9, id_carta10, id_carta11) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-            (objeto.get_id_equipo(), objeto.get_nombre(), objeto.get_id_usuario()),
+            "INSERT INTO equipo (id_equipo, id_usuario, nombre_equipo, id_carta1, id_carta2, id_carta3, id_carta4, id_carta5, id_carta6, id_carta7, id_carta8, id_carta9, id_carta10, id_carta11) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+            (
+                objeto.get_id_equipo(),
+                objeto.get_nombre(),
+                objeto.get_id_usuario(),
+                objeto.get_id_carta1(),
+                objeto.get_id_carta2(),
+                objeto.get_id_carta3(),
+                objeto.get_id_carta4(),
+                objeto.get_id_carta5(),
+                objeto.get_id_carta6(),
+                objeto.get_id_carta7(),
+                objeto.get_id_carta8(),
+                objeto.get_id_carta9(),
+                objeto.get_id_carta10(),
+                objeto.get_id_carta11(),
+            ),
         )
 
     def actualizar(self, objeto):  # FUNCIONA
         self.__database.execute_non_query(
-            "UPDATE equipo SET nombre_equipo = ?, baja_equipo = ? WHERE id_equipo = ?",
-            (objeto.get_nombre(), objeto.get_baja_equipo(), objeto.get_id_equipo()),
+            "UPDATE equipo SET nombre_equipo = ?, baja_equipo = ?, id_carta1 = ?, id_carta2 = ?, id_carta3 = ?, id_carta4 = ?, id_carta5 = ?, id_carta6 = ?, id_carta7 = ?, id_carta8 = ?, id_carta9 = ?, id_carta10 = ?, id_carta11 = ? WHERE id_equipo = ?",
+            (
+                objeto.get_nombre(),
+                objeto.get_baja_equipo(),
+                objeto.get_id_equipo(),
+                objeto.get_id_carta1(),
+                objeto.get_id_carta2(),
+                objeto.get_id_carta3(),
+                objeto.get_id_carta4(),
+                objeto.get_id_carta5(),
+                objeto.get_id_carta6(),
+                objeto.get_id_carta7(),
+                objeto.get_id_carta8(),
+                objeto.get_id_carta9(),
+                objeto.get_id_carta10(),
+                objeto.get_id_carta11(),
+            ),
         )
 
     def borrar(self, id):  # FUNCIONA
--- src/model/database/Equipo.py
+++ src/model/database/Equipo.py
@@ -1,9 +1,36 @@
 class Equipo:
-    def __init__(self, id_equipo, nombre_equipo, id_usuario):
+    def __init__(
+        self,
+        id_equipo,
+        nombre_equipo,
+        id_usuario,
+        id_carta1,
+        id_carta2,
+        id_carta3,
+        id_carta4,
+        id_carta5,
+        id_carta6,
+        id_carta7,
+        id_carta8,
+        id_carta9,
+        id_carta10,
+        id_carta11,
+    ):
         self.__id_equipo: int = id_equipo
         self.__nombre_equipo: str = nombre_equipo
         self.__id_usuario: int = id_usuario
         self.__baja_equipo = 0
+        self.__id_carta1 = id_carta1
+        self.__id_carta2 = id_carta2
+        self.__id_carta3 = id_carta3
+        self.__id_carta4 = id_carta4
+        self.__id_carta5 = id_carta5
+        self.__id_carta6 = id_carta6
+        self.__id_carta7 = id_carta7
+        self.__id_carta8 = id_carta8
+        self.__id_carta9 = id_carta9
+        self.__id_carta10 = id_carta10
+        self.__id_carta11 = id_carta11
 
     def get_id_equipo(self):
         return self.__id_equipo
@@ -16,3 +43,36 @@ class Equipo:
 
     def get_baja_equipo(self):
         return self.__baja_equipo
+
+    def get_id_carta1(self):
+        return self.__id_carta1
+
+    def get_id_carta2(self):
+        return self.__id_carta2
+
+    def get_id_carta3(self):
+        return self.__id_carta3
+
+    def get_id_carta4(self):
+        return self.__id_carta4
+
+    def get_id_carta5(self):
+        return self.__id_carta5
+
+    def get_id_carta6(self):
+        return self.__id_carta6
+
+    def get_id_carta7(self):
+        return self.__id_carta7
+
+    def get_id_carta8(self):
+        return self.__id_carta8
+
+    def get_id_carta9(self):
+        return self.__id_carta9
+
+    def get_id_carta10(self):
+        return self.__id_carta10
+
+    def get_id_carta11(self):
+        return self.__id_carta11
