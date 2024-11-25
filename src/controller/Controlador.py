6a17340bdbb10a78b71c32f6dd05310c89577dd2
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2024-11-25 03:30:10 -0300

    Restricciones
    Restricciones para que solo se pueda ingresar un solo equipo y un solo usuario, y que lo oblige a ingresarlos

--- src/controller/Controlador.py
+++ src/controller/Controlador.py
@@ -11,18 +11,16 @@ from abc import ABC, abstractmethod
 class Controlador(ABC):
     def __init__(self):
         self._view = None
-        self.boton_actual = None    
 
     @abstractmethod
     def manejar_eventos(self, eventos, mouse_pos):
         pass
-    
-    def cambiar_boton_actual (self):
+
+    def cambiar_boton_actual(self):
         botones = self._view.get_botones()
         for boton_texto, boton in botones.items():
-                    if boton.seleccionado:
-                        self.boton_actual = boton
-    
+            if boton.seleccionado:
+                self.boton_actual = boton
 
     def main_loop(self):
         while True:
@@ -31,10 +29,5 @@ class Controlador(ABC):
                 self._view.mostrar()  # Mostrar el menú
                 eventos = pygame.event.get()  # Manejar eventos
                 self.manejar_eventos(eventos, mouse_pos)
-
-                if self.boton_actual is not None:
-                        self.boton_actual.changeColor()
-                        self.boton_actual.update(self._view._pantalla)
-                        
                 clock.tick(60)
                 pygame.display.update()
